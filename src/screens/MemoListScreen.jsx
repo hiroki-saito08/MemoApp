1e977e3a4d424bbd19974ebb7d9030252c443410
saitohiroki <hirokisaito0813@gmail.com>
2022-04-14 00:02:20 +0900

    Auto login and logout button

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import {
+  Text, View, StyleSheet, TouchableOpacity, Alert,
+} from 'react-native';
+
+import firebase from 'firebase';
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
+// import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  const handlePress = () => {
+    firebase.auth().signOut()
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'LogIn' }],
+        });
+      })
+      .catch(() => {
+        Alert.alert('ログアウトに失敗しました');
+      });
+  };
+
   return (
     <View style={styles.container}>
       <MemoList />
@@ -12,6 +30,25 @@ export default function MemoListScreen(props) {
         name="plus"
         onPress={() => { navigation.navigate('MemoCreate'); }}
       />
+      <TouchableOpacity
+        onPress={handlePress}
+        style={{
+          paddingHorizontal: 12,
+          paddingVertical: 4,
+          backgroundColor: '#467fd3',
+          width: 100,
+          marginTop: 10,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 14,
+            color: 'rgba( 255, 255, 255, 0.7)',
+          }}
+        >
+          ログアウト
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
