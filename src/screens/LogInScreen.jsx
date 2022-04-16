0e8aba24c079eaac8c0dc5d85ec65e431cf2cea5
saitohiroki <hirokisaito0813@gmail.com>
2022-04-16 10:04:19 +0900

    Create Loaadng component

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -5,11 +5,13 @@ import {
 import firebase from 'firebase';
 
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 export default function LogInScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isloading, setIsloading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
@@ -18,12 +20,15 @@ export default function LogInScreen(props) {
           index: 0,
           routes: [{ name: 'MemoList' }],
         });
+      } else {
+        setIsloading(false);
       }
     });
     return unsubscribe;
   }, []);
 
   const handlePress = () => {
+    setIsloading(true);
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
         const { user } = userCredential;
@@ -35,11 +40,15 @@ export default function LogInScreen(props) {
       })
       .catch((error) => {
         Alert.alert(error.code);
-      });
+      })
+      .then(() => {
+        setIsloading(false);
+      })
   };
 
   return (
     <View style={StyleSheet.container}>
+      <Loading isloading={isloading} />
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
         <TextInput
